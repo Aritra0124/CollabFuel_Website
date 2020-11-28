29a1632cd106ef4bf6873993c24d53c04024e230
aritra <aritrachakraborty0124@gmail.com>
2020-11-28 13:31:41 +0530

    FIXING SOME ISSUES

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/coaching")
+def training():
+    return render_template('coaching.html')
+
+
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run()
