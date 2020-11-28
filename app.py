33d3e38beb1119be34d3041cbd859374fea88d83
Aritra Chakraborty <46535948+aritra0124@users.noreply.github.com>
2020-11-28 15:15:33 +0530

    Sceleton ready page

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/coaching")
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/training")
 def training():
     return render_template('coaching.html')
-
-
 
 
 @app.route("/")
---
+++ static/js/hide-n-show.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.blue_bt').click(function () {
+        $('#cours1').show();
+        $('#cours2').show();
+        $('#cours3').show();
+
+    });
+});
+
